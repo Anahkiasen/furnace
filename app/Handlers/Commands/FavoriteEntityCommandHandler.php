651e8d2b13aee05c56b6b93e1932c57954151dc9
Maxime Fabre <ehtnam6@gmail.com>
2015-04-18 15:35:30 +0200

    Add favorites toggle

--- app/Handlers/Commands/FavoriteEntityCommandHandler.php
+++ app/Handlers/Commands/FavoriteEntityCommandHandler.php
@@ -1,7 +1,9 @@
 <?php namespace Furnace\Handlers\Commands;
 
 use Furnace\Commands\FavoriteEntityCommand;
+use Furnace\Entities\Interfaces\Favoritable;
 use Furnace\Entities\Models\Favorite;
+use Furnace\Entities\Models\User;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class FavoriteEntityCommandHandler
@@ -41,11 +43,31 @@ class FavoriteEntityCommandHandler
             throw new ModelNotFoundException;
         }
 
-        $favorite = $this->repository->create([
+        $favorite = $this->toggleFavorite($command->user, $entity);
+
+        return $favorite;
+    }
+
+    /**
+     * @param User        $user
+     * @param Favoritable $entity
+     *
+     * @return Favorite|null
+     */
+    protected function toggleFavorite(User $user, Favoritable $entity)
+    {
+        $attributes = [
             'favoritable_type' => get_class($entity),
             'favoritable_id'   => $entity->id,
-            'user_id'          => $command->user->id,
-        ]);
+            'user_id'          => $user->id,
+        ];
+
+        // Toggle favorite
+        if ($user->hasFavorited($entity)) {
+            $favorite = $this->repository->where($attributes)->delete();
+        } else {
+            $favorite = $this->repository->create($attributes);
+        }
 
         return $favorite;
     }
