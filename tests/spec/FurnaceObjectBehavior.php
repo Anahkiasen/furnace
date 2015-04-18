ce50cddf824f0f6ef7c9fc19933d57b9d2a9c88e
Maxime Fabre <ehtnam6@gmail.com>
2015-04-18 18:28:30 +0200

    Mock ignition during tests

--- tests/spec/FurnaceObjectBehavior.php
+++ tests/spec/FurnaceObjectBehavior.php
@@ -4,7 +4,9 @@ namespace spec\Furnace;
 use Furnace\Entities\Models\Track;
 use Furnace\Entities\Models\Tracker;
 use Furnace\Entities\Models\User;
+use Furnace\Services\Ignition;
 use League\FactoryMuffin\Facade;
+use Mockery;
 use PhpSpec\Laravel\LaravelObjectBehavior;
 
 class FurnaceObjectBehavior extends LaravelObjectBehavior
@@ -15,6 +17,10 @@ class FurnaceObjectBehavior extends LaravelObjectBehavior
     public function let()
     {
         $this->loadFactories();
+
+        app()->bind(Ignition::class, function () {
+           return Mockery::mock(Ignition::class)->shouldReceive('complete')->andReturn([])->getMock();
+        });
 
         User::first() ?: Facade::create(User::class);
     }
