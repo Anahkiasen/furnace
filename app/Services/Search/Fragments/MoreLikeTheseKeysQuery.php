795146b3dec37267296ff08e357396c604ddf7e4
Maxime Fabre <ehtnam6@gmail.com>
2015-04-27 14:00:27 +0200

    Apply boosts

---
+++ app/Services/Search/Fragments/MoreLikeTheseKeysQuery.php
@@ -0,0 +1,28 @@
+<?php
+namespace Furnace\Services\Search\Fragments;
+
+use ElasticSearcher\Abstracts\AbstractFragment;
+
+class MoreLikeTheseKeysQuery extends AbstractFragment
+{
+
+    /**
+     * MoreLikeTheseKeysQuery constructor.
+     *
+     * @param integer[] $keys
+     * @param string[]  $fields
+     * @param int       $boost
+     */
+    public function __construct(array $keys, $fields, $boost = 1)
+    {
+        $this->body = [
+            'more_like_this' => [
+                'fields'        => $fields,
+                'ids'           => $keys,
+                'min_term_freq' => 1,
+                'min_doc_freq'  => 1,
+                'boost'         => $boost,
+            ]
+        ];
+    }
+}
--- app/Services/Search/Queries/SimilarTracksQuery.php
+++ app/Services/Search/Queries/SimilarTracksQuery.php
@@ -4,11 +4,23 @@ namespace Furnace\Services\Search\Queries;
 use ElasticSearcher\Abstracts\AbstractQuery;
 use Furnace\Services\ScoreComputer;
 use Furnace\Services\Search\Fragments\ExcludeKeysFilter;
+use Furnace\Services\Search\Fragments\MoreLikeTheseKeysQuery;
 use Furnace\Services\Search\Fragments\ScoreFilter;
 use Furnace\Services\Search\ResultsParsers\TracksResultsParser;
 
 class SimilarTracksQuery extends AbstractQuery
 {
+    /**
+     * The field to look for similarities in.
+     *
+     * @type array
+     */
+    protected $fields = [
+        'artist'  => 1,
+        'tracker' => 1.5,
+        'tags'    => 2,
+    ];
+
     /**
      * Prepare the query. Add filters, sorting, ....
      */
@@ -18,18 +30,19 @@ class SimilarTracksQuery extends AbstractQuery
         $this->parseResultsWith(new TracksResultsParser());
         $tracks = $this->getData('tracks');
 
+        // Build queries
+        $queries = [];
+        foreach ($this->fields as $field => $boost) {
+            $queries[] = new MoreLikeTheseKeysQuery($tracks, [$field], $boost);
+        }
+
         $this->body['size']  = 25;
         $this->body['query'] = [
             'filtered' => [
                 'query'  => [
-                    'more_like_this' => [
-                        'fields'        => [
-                            'tags'
-                        ],
-                        'ids'           => $tracks,
-                        'min_term_freq' => 1,
-                        'min_doc_freq'  => 1
-                    ]
+                    'dis_max' => [
+                        'queries' => $queries,
+                    ],
                 ],
                 'filter' => [
                     'and' => [
