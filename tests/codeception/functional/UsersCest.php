d0a94952ee2b7a4bcb7e1fbcd75c17d0357a2a27
Maxime Fabre <ehtnam6@gmail.com>
2015-05-09 14:10:40 +0200

    Add basic login test

---
+++ tests/codeception/functional/UsersCest.php
@@ -0,0 +1,17 @@
+<?php
+class UsersCest
+{
+    public function testCanLogin(FunctionalTester $I)
+    {
+        $I->amOnRoute('home');
+        $I->click('Login / Signup');
+        $I->seeCurrentRouteIs('users.create');
+        $I->submitForm('#login', [
+            'name' => 'Anahkiasen',
+            'password' => 'furnace',
+        ]);
+
+        $I->seeCurrentUrlEquals('');
+        $I->seeAuthentication();
+    }
+}
